841d6696fdc4919334b7d7ac160ebad9e576e230
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-04 21:07:07 +0200

    input search removed to own component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './components/card-list/card-list.component';
+import Search from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -32,12 +33,10 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <input
-          name='search'
-          type='search'
+        <Search
+          search={this.state.search}
+          SearchMonster={this.handleSearch}
           placeholder='search monster'
-          value={this.state.search}
-          onChange={this.handleSearch}
         />
         <CardList monsters={searchResult} />
       </div>
